6af4993112e9421bbd3607d1db6d2bbcc6774abb
Szymon Studniarz <szymek1019@gmail.com>
2021-12-19 22:30:34 +0100

    generate form for product type

--- src/warehouseManager.js
+++ src/warehouseManager.js
@@ -9,11 +9,14 @@ class warehouseManager extends elementToolsLib {
             document.getElementById("contentContainer").remove();
         }
     }
+    capitalize(str) {
+        return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+    }
     generateNavMenu(active) {
         const elems = new Array('main', 'products', 'categories');
         this.renderElem("div", "navMenu", null, "mainContainer", null);
         for (let elem of elems) {
-            let item = this.renderElem("button", `${elem}PageButton`, "navButton", "navMenu", `${elem.charAt(0).toUpperCase()}${elem.slice(1)}`);
+            let item = this.renderElem("button", `${elem}PageButton`, "navButton", "navMenu", this.capitalize(elem));
             item.setAttribute("onclick", `w1.generatePage('${elem}')`);
             item.setAttribute("class", "navButton");
         }
@@ -24,8 +27,8 @@ class warehouseManager extends elementToolsLib {
         this.generateNavMenu(active);
         this.renderElem("div", "contentContainer", null, "mainContainer", null);
         if (active === 'products') {
-            const headerFields = new Array("Name", "Category", "Price", "In stock", "Description");
             this.renderElem("div", "storage", "dbContent", "contentContainer", null);
+            const headerFields = new Array("Name", "Category", "Price", "In stock", "Description");
             this.renderElem("div", "buttonsContainer", null, "contentContainer", null);
             this.renderElem("button", "addItem", "managementButtons", "buttonsContainer", "Add Item").setAttribute("onclick","w1.addItem('Product')");
             this.renderElem("button", "removeItem", "managementButtons", "buttonsContainer", "Remove Item");
@@ -46,9 +49,43 @@ class warehouseManager extends elementToolsLib {
                 document.getElementById(`i${index}categoryID`).innerText = this.warehouse.Categories[this.warehouse.Products[index].categoryID].name;
             }
         }
+        else if (active === 'categories') {
+            
+        }
     }
     addItem(type) {
         this.openPopUp();
-        this.renderElem("form", `add${type}`, null, "popUpContainer", null);
+        this.renderElem("form", `add${type}`, "addItemForm", "popUpContainer", null);
+        let inputType;
+        if (type.toUpperCase() === "PRODUCT") {
+            inputType = new Product();
+        }
+        else {
+            inputType = new Category();
+        }
+        for (const index in inputType) {
+            this.renderElem("label", `${index}Label`, null, `add${type}`, index).setAttribute("for", `${index}`);
+            if (index != "categoryID") {   
+                let elem = this.renderElem("input", `${index}`, "formInput", `add${type}`, null);
+                elem.required = true;
+                elem.setAttribute("type", "text");
+            } else {
+                let select = this.renderElem("select", `${index}`, null, `add${type}`, null);
+                select.innerHTML += "<option disabled selected hidden value=-1>- Pick category -</option>";
+                for (const obj in this.warehouse.Categories) {
+                    select.innerHTML += `<option value=${obj}>${this.warehouse.Categories[obj].name}</option>`;   
+                }
+            }
+        }
+        if (type.toUpperCase() === "PRODUCT") {
+            //document.getElementById("categoryID").setAttribute("type", "number");
+            let howManyAvailable = document.getElementById("howManyAvailable");
+            howManyAvailable.setAttribute("type", "number");
+            howManyAvailable.setAttribute("min", "0");    
+        }
+        document.getElementById("description").required = false;
+        let submitButton = this.renderElem("input", "submitButton", null, `add${type}`, null);
+        submitButton.setAttribute("type", "submit");
+        submitButton.value = "Add Item";
     }
  }
